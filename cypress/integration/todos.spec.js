711b9339d5415514d8e09d8fdd4461250edbb15b
Usuario invitado <Guest@jon-mac.local>
2020-05-28 12:15:27 +0200

    clear from last branch

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -2,22 +2,11 @@ describe("Todo app", () => {
   it("loads the page", () => {
     cy.visit("/");
 
-    cy.log("About to fetch the first element");
-
-    cy.wrap(5).should("eq", 5);
-
     cy.get("[data-cy=todo-item-3]")
-      //.then((element) => { debugger; }) //harder way
-      .debug()
       .should("have.text", "Hello world")
       .should("not.have.class", "completed")
       .find(".toggle")
       .should("not.be.checked");
-
-    //debugger; //If I put it here, it will be the first that loads, as it is "asynchronous"
-    // the queue would be:
-    //      [visit, get, should, should, find, should, get, ...]
-    //DEBUG
 
     cy.get("[data-cy=todo-item-4]")
       .should("have.text", "Goodnight moon")
