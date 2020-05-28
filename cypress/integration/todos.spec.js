4fb41a8e7d57ab672df123b9ea59116044a6442a
Usuario invitado <Guest@jon-mac.local>
2020-05-28 11:37:50 +0200

    differences with different selectors

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,9 +1,20 @@
 describe('Todo app', ()=>{
   it('loads the page', ()=>{
     cy.visit('/');
-    cy.get('.todo-list li:nth-child(1)').should('have.text', 'Hello world')//it's case sensitive
+    
+    //cy.contains('Hello world') // This gives false positives (look .contains('Goodnight))
+    //This is the best (and easiest) way
+    cy.get('[data-cy=todo-item-3]')
+      .should('have.text', 'Hello world')
       .should('not.have.class', 'completed')
       .find('.toggle')
       .should('not.be.checked');
+
+    //this one works, but I have to be a lot more careful, and won't work it there are any changes in the DOM
+    cy.contains('Goodnight moon')
+      .should('have.text', 'Goodnight moon')
+      .siblings('input')
+      //.find('.toggle') // I ca't look for a toggle inside a toggle
+      .should('be.checked');
   });
 });
