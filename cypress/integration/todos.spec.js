c6b955541ecfbf499fc89eec8f103e1382949983
Usuario invitado <Guest@jon-mac.local>
2020-05-28 11:53:05 +0200

    revised best practices commit

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -2,19 +2,18 @@ describe('Todo app', ()=>{
   it('loads the page', ()=>{
     cy.visit('/');
     
-    //cy.contains('Hello world') // This gives false positives (look .contains('Goodnight))
-    //This is the best (and easiest) way
     cy.get('[data-cy=todo-item-3]')
       .should('have.text', 'Hello world')
       .should('not.have.class', 'completed')
       .find('.toggle')
       .should('not.be.checked');
 
-    //this one works, but I have to be a lot more careful, and won't work it there are any changes in the DOM
     cy.contains('Goodnight moon')
       .should('have.text', 'Goodnight moon')
-      .siblings('input')
-      //.find('.toggle') // I ca't look for a toggle inside a toggle
+      .parent('div')
+      .parent('li')
+      .should('have.class', 'completed')
+      .find('.toggle')
       .should('be.checked');
   });
 });
