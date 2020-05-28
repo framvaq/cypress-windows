6f40f85c84b71815d88778b5ea14c47516baa69d
Usuario invitado <Guest@jon-mac.local>
2020-05-28 12:10:22 +0200

    added log and debug functions

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,19 +1,28 @@
-describe('Todo app', ()=>{
-  it('loads the page', ()=>{
-    cy.visit('/');
-    
-    cy.get('[data-cy=todo-item-3]')
-      .should('have.text', 'Hello world')
-      .should('not.have.class', 'completed')
-      .find('.toggle')
-      .should('not.be.checked');
+describe("Todo app", () => {
+  it("loads the page", () => {
+    cy.visit("/");
 
-    cy.contains('Goodnight moon')
-      .should('have.text', 'Goodnight moon')
-      .parent('div')
-      .parent('li')
-      .should('have.class', 'completed')
-      .find('.toggle')
-      .should('be.checked');
+    cy.log("About to fetch the first element");
+
+    cy.wrap(5).should("eq", 5);
+
+    cy.get("[data-cy=todo-item-3]")
+      //.then((element) => { debugger; }) //harder way
+      .debug()
+      .should("have.text", "Hello world")
+      .should("not.have.class", "completed")
+      .find(".toggle")
+      .should("not.be.checked");
+
+    //debugger; //If I put it here, it will be the first that loads, as it is "asynchronous"
+    // the queue would be:
+    //      [visit, get, should, should, find, should, get, ...]
+    //DEBUG
+
+    cy.get("[data-cy=todo-item-4]")
+      .should("have.text", "Goodnight moon")
+      .should("have.class", "completed")
+      .find(".toggle")
+      .should("be.checked");
   });
 });
