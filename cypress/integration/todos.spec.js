ee76dcbe0277e6bd1c5329baf0be32cd2d5850b3
framvaq <francisco.ramirezvaquero@gmail.com>
2020-05-29 13:55:54 +0200

    post method not working

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,8 +1,25 @@
 /// <reference types="Cypress" />
 
-it('loads the page', () => {
-  describe('Todo app', () => {
+describe('Todo app', () => {
+  it('loads the page', () => {
+    /*   cy.server();
+
+    cy.route('/api/todos', [
+      {
+        text: 'Hello world',
+        completed: false,
+        id: 3
+      },
+      {
+        id: 4,
+        completed: true,
+        text: 'Goodnight moon'
+      }
+    ]).as('preload');
+
     cy.visit('/');
+
+    cy.wait('@preload');
 
     cy.get('[data-cy=todo-item-3]')
       .should('have.text', 'Hello world')
@@ -15,5 +32,35 @@ it('loads the page', () => {
       .should('have.class', 'completed')
       .find('.toggle')
       .should('be.checked');
+
+    //Turn off to restore to defaults, then restart again
+    cy.server({ enable: false });
+    cy.log('server shutdown');
+*/
+    //Doesn't work, cypress sends a normal (not stubbed) get request
+    cy.server({ method: 'POST' });
+
+    //Doesn't work, cypress sends a normal (not stubbed) get request
+    cy.route('POST', '/api/todos/3', [
+      {
+        text: 'Hello worlddddddddddddd',
+        completed: false,
+        id: 3
+      },
+      {
+        id: 4,
+        completed: true,
+        text: 'Goodnight moon'
+      }
+    ]).as('update');
+
+    cy.visit('/');
+
+    cy.wait('@update');
+
+    cy.get('[data-cy=todo-item-3]').find('label').dblclick();
+    cy.get('.editing').find('input').type('zzz');
+    cy.get('h1').click();
+    cy.get('[data-cy=todo-item-3]').should('have.text', 'Hello worldzzz');
   });
 });
