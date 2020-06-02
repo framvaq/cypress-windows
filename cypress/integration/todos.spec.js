a4fb8e69d662b0e4c40d0f4cf28ff8af99ead1c2
framvaq <francisco.ramirezvaquero@gmail.com>
2020-06-02 10:48:13 +0200

    mocked network retries

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 describe('Todo app', () => {
   beforeEach(function () {
-    cy.fixture('todos/all').as('todosPreload');
+    cy.fixture('todos/all').as('todos');
   });
   //VERY IMPORTANT TO USE THE 'OLD SCHOOL' FUNCTION SYNTAX, NOT THE NEW ARROW I.E.
   /*
@@ -40,18 +40,58 @@ describe('Todo app', () => {
     });
   });
 
-  it.only('Todo creation retries', function () {
+  context.only('Todo creation retries', function () {
     beforeEach(function () {
       cy.server();
       // Alias the fixture data
       cy.route('/api/todos', '@todos').as('preload');
       cy.visit('/');
       cy.wait('@preload');
+
+      cy.route({
+        method: 'POST',
+        url: 'api/todos',
+        status: 500,
+        response: ''
+      }).as('createTodo');
+
+      cy.get('[data-cy=new-todo]').type('new todo{enter}');
+
+      cy.wait('@createTodo');
+
+      cy.route({
+        method: 'POST',
+        url: 'api/todos',
+        status: 500,
+        response: ''
+      }).as('createTodo2');
+
+      cy.wait('@createTodo2');
     });
-    cy.visit('/');
 
-    it('retries 3 times', function () {});
+    it('retries 3 times', function () {
+      cy.route({
+        method: 'POST',
+        url: 'api/todos',
+        status: 201,
+        response: ''
+      }).as('createTodo3');
 
-    it('fails after 3 unsuccessful attempts', function () {});
+      cy.wait('@createTodo3');
+
+      cy.get('[data-cy=todo-list]').children().should('have.length', 3);
+    });
+
+    it('fails after 3 unsuccessful attempts', function () {
+      cy.route({
+        method: 'POST',
+        url: 'api/todos',
+        status: 500,
+        response: ''
+      }).as('createTodo3');
+
+      cy.wait('@createTodo3');
+      cy.get('[data-cy=todo-list]').children().should('have.length', 2);
+    });
   });
 });
